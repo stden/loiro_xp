20f7da91ce38ea44f34a1582bd986786ab254761
Denis Stepulenok <super.denis@gmail.com>
2013-01-18 13:46:09 +0400

    Local changes

--- do.bat
+++ do.bat
@@ -1,9 +1,13 @@
 @echo off
+git config --global user.email "super.denis@gmail.com"
+git config --global user.name "Denis Stepulenok"
+git commit -a -m"Local changes"
 git checkout master
-git clean -f
 git pull
 echo Создать пользователя olymp с паролем olymp
 net user olymp olymp /ADD
 echo Создание каталога C:\work
 rmdir C:\work /q/s
 mkdir C:\work
+rmdir C:\.dist /q/s
+rmdir C:\olymp /q/s
