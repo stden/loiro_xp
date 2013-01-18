c0b58837d342249e73c99e1b39ad73959cea7501
Denis Stepulenok <super.denis@gmail.com>
2013-01-18 17:15:24 +0400

    DESKTOP4

--- do.bat
+++ do.bat
@@ -2,6 +2,7 @@
 git config --global user.email "super.denis@gmail.com"
 git config --global user.name "Denis Stepulenok"
 git config --global push.default simple
+git add .
 git commit -a -m"%COMPUTERNAME%"
 git checkout master
 git pull
