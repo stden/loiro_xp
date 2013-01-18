e23cac0e4b9e8cdb80e7c586e1cfac9d5f7aa2b3
Denis Stepulenok <super.denis@gmail.com>
2013-01-18 20:15:32 +0400

    32-09

--- do.bat
+++ do.bat
@@ -10,7 +10,7 @@ git commit -a -m"%COMPUTERNAME%"
 git checkout master
 git pull
 git clean -f
-git push
+rem git push
 
 echo Создать пользователя olymp с паролем olymp
 net user olymp olymp /ADD
@@ -42,4 +42,4 @@ attrib "%ALLUSERSPROFILE%\Desktop\desktop.ini" +s +h
 
 reg add "HKEY_CLASSES_ROOT\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /v "LocalizedString" /t REG_EXPAND_SZ /d "192.168.%CLASS%.%COMP% %USERNAME%" /f
 
-netsh interface ip set address name="Local Area Connection" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
+rem netsh interface ip set address name="Local Area Connection" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
