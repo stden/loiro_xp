181272c61feee33030d1216d457778a8f763255b
Denis Stepulenok <super.denis@gmail.com>
2013-01-19 09:41:05 +0400

    31-04

--- ip.bat
+++ ip.bat
@@ -1,2 +1,2 @@
 netsh interface ip set address name="Local Area Connection" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
-netsh interface ip set address name="╧юфъы■ўхэшх яю ыюъры№эющ ёхЄш" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
+netsh interface ip set address name="Подключение по локальной сети" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
